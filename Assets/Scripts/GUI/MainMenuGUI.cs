93481836bb2ab6359d3b18da2f4adf49ce34e39f
Sami Kojo <sami@aeonic-entertainment.com>
2016-09-30 14:49:56 +0300

    Initial commit

---
+++ Assets/Scripts/GUI/MainMenuGUI.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using GameProgramming2D.State;
+using System.Collections;
+
+namespace GameProgramming2D.GUI
+{
+    public class MainMenuGUI : MonoBehaviour
+    {
+        public void OnStartGamePressed()
+        {
+            GameManager.Instance.StateManager
+                .PerformTransition ( TransitionType.MainMenuToGame );
+        }
+
+        public void OnExitGamePressed()
+        {
+            Application.Quit ();
+        }
+    }
+}
---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace GameProgramming2D
+{
+    public class InputManager : MonoBehaviour
+    {
+        private void Update ()
+        {
+            if ( Input.GetKeyUp ( KeyCode.P ) )
+            {
+                // Pause game
+                GameManager.Instance.Pauser.TogglePause ();
+            }
+
+            HandlePlayerInputs ();
+        }
+
+        private static void HandlePlayerInputs ()
+        {
+            if( GameManager.Instance.Player == null)
+            {
+                return;
+            }
+
+            if ( Input.GetButtonDown ( "Jump" ) )
+            {
+                GameManager.Instance.Player.Jump = true;
+            }
+
+            if ( Input.GetButtonDown ( "Fire1" ) )
+            {
+                GameManager.Instance.Player.Gun.Fire ();
+            }
+
+            if ( Input.GetButtonDown ( "Fire2" ) )
+            {
+                GameManager.Instance.Player.LayBomb ();
+            }
+
+            var horizontal = Input.GetAxis ( "Horizontal" );
+            GameManager.Instance.Player.SetHorizontal ( horizontal );
+        }
+    }
+}
